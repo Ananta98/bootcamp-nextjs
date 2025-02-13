0c0364a7c3730637e49f362bd27ef16e874d4bc6
Ananta98 <kusumaananta042@gmail.com>
2025-02-13 11:21:01 +0700

    reinit git

---
+++ nexts-sb-app-router/layout/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Geist, Geist_Mono } from 'next/font/google'
+import Link from 'next/link'
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-blue-600 text-white p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1 className="text-xl font-bold">Next.js App</h1>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main className="flex-1 container mx-auto p-4">{children}</main>
+      <footer className="bg-gray-800 text-white p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} My Personal Website</p>
+      </footer>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ nexts-sb-pages-router/pages/about/me/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Me = () => {
+  return <div>Me page</div>
+}
+
+export default Me
